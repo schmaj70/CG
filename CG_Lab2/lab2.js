f32d676e1eac46b195a1bbd69108f0443abe87cc
Jeff <schmadebeck@fitmentindustries.com>
2020-11-09 20:22:01 -0600

    edit comments

--- CG_Lab2/lab2.js
+++ CG_Lab2/lab2.js
@@ -4,13 +4,19 @@
  * Create a 3d "W" 
  * 
  * Make it rotate on the screen accross various axes 
- * Use  the  lookAt  function  to  change  the  default  position  of  the  camera  on  startup 
- * create the projection matrix and send it to the vertex shader 
- * Make it be created with different colors for a 3d effect
- * Make it have one or more translations 
- * Make it have one or more scalings
- * Do something "creative", I chose to make the "W" be colored in randomly each time
  * 
+ * Use  the  lookAt  function  to  change  the  default  position  of  the  camera  on  startup 
+ * 
+ * create the projection matrix and send it to the vertex shader 
+ * 
+ * Make it be created with different colors for a 3d effect
+ * 
+ * Make it have one or more translations
+ *  
+ * Make it have one or more scalings
+ * 
+ * Do something "creative", I chose to make the "W" be colored in randomly each time, and added
+ * the ability to move around the camera 
  */
 var canvas;
 var gl;
